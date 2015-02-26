183602d70e04f3a708b0ee53668bcc3e9ab8b2c0
hifilorau <tomrau@gmail.com>
2015-02-26 15:43:45 -0500

    adding controller

---
+++ src/new-shares/new-share-controller.js
@@ -0,0 +1,25 @@
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.when('/bikes/new', {
+    controller: 'NewBikeCtrl',
+    controllerAs: 'vm',
+    templateUrl: 'bikes/new-bike.html'
+  });
+}]).controller('NewBikeCtrl', ['$location', 'Bike', 'bikeStore', function($location, Bike, bikeStore) {
+  var self = this;
+
+  self.bike = Bike();
+
+  self.doneEditing = function () {
+    bikeStore.add(self.bike);
+    self.goToBikes();
+  };
+
+  self.cancelEditing = function () {
+    self.goToBikes();
+  };
+
+  self.goToBikes = function () {
+    $location.path('/bikes');
+  };
+
+}]);
