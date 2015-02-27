78f9d105caf2234f0938de816490cbf2b394e321
hifilorau <tomrau@gmail.com>
2015-02-27 14:37:45 -0500

    workign save

--- src/shares/shares-controller.js
+++ src/shares/shares-controller.js
@@ -2,13 +2,23 @@ app.config(['$routeProvider', function($routeProvider) {
   var routeDefinition = {
     templateUrl: 'shares/shares.html',
     controller: 'SharesCtrl',
-    controllerAs: 'vm'
+    controllerAs: 'vm',
+    resolve: {
+      shares: ['shareService', function (shareService) {
+        return shareService.getShareList();
+      }]
+    }
   };
 
   $routeProvider.when('/', routeDefinition);
   $routeProvider.when('/shares', routeDefinition);
 }])
-.controller('SharesCtrl', 'shareService' [function (SharesCtrl, sharesService) {
+.controller('SharesCtrl', ['shares', 'shareService', 'Share', function (shares, shareService, Share) {
   // TODO: load these via AJAX
-  this.shares = [];
+
+var self = this;
+
+self.shares = shares;
+
+
 }]);
